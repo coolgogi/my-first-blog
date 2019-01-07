43ff88b1998fa7889f342290c7f919e4fd000b95
Your name <suhyun926@naver.com>
2019-01-07 14:08:17 +0900

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,6 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    def __str__(self):
-	return self.title
 
 def publish(self):
     self.published_date = timezone.now()
